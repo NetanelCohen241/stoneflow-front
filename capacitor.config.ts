a8c43f2f13a7c63451cabed3a8811186a4c13230
Netanel Cohen <36200849+NetanelCohen241@users.noreply.github.com>
2025-07-31 08:34:46 +0300

    Add Capacitor config for iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.example.stoneflow',
+  appName: 'Stoneflow',
+  webDir: 'dist',
+  bundledWebRuntime: false
+}
+
+export default config
